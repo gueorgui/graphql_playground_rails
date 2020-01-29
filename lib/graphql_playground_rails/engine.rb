e693aceffdc9edf50f5487808159f1e2776e98cd
Gueorgui Tcherednitchenko <gt@gueorgui.net>
2020-01-29 11:08:42 +0000

    change: Remove Webpacker

--- lib/graphql_playground_rails/engine.rb
+++ lib/graphql_playground_rails/engine.rb
@@ -1,28 +1,5 @@
 module GraphqlPlaygroundRails
   class Engine < ::Rails::Engine
     isolate_namespace GraphqlPlaygroundRails
-
-    initializer 'webpacker.proxy' do |app|
-      insert_middleware = begin
-                            GraphqlPlaygroundRails.webpacker.config.dev_server.present?
-                          rescue
-                            nil
-                          end
-      next unless insert_middleware
-
-      app.middleware.insert_before(
-        0, Webpacker::DevServerProxy,
-        ssl_verify_none: true,
-        webpacker: GraphqlPlaygroundRails.webpacker
-      )
-    end
-
-    root_dir = File.dirname(__dir__)
-    public_dir = File.join(root_dir, 'public')
-    puts "Mounting middleware at root = #{public_dir}"
-    config.app_middleware.use(
-      Rack::Static,
-      urls: ['/playground'], root: public_dir
-    )
   end
 end
